234654785cb1d8a82d6f9d0ff8a3d357f253d4a5
southppp22 <tnddl032@gmail.com>
2021-02-23 21:57:32 +0900

    [Server]Add signUp api
    회원가입 로직 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from 'src/entity/User.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {
+    this.usersService = usersService;
+  }
+
+  @Post('signUp')
+  async saveUser(@Body() user: User): Promise<string> {
+    await this.usersService.saveUser(user);
+    return Object.assign({
+      data: null,
+      statusCode: 201,
+      statusMsg: 'saved successfully',
+    });
+  }
+}
