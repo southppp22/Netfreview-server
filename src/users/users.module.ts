1ca6ab27b7c780b28a8bf2bd012f52338475c770
southppp22 <tnddl032@gmail.com>
2021-02-23 21:56:10 +0900

    [Server]Add User entity in users.modules
    users.module에 User 엔티티 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/User.entity';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
