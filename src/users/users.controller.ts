97b9ed07962d15436e0796f1df13504a8e201a09
southppp22 <tnddl032@gmail.com>
2021-02-25 14:59:05 +0900

    [Server]Add SignIn API
    로그인 api 구현(스케치)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,61 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Post,
+  Request,
+  Response,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
 import { User } from 'src/entity/User.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersService: UsersService) {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {
     this.usersService = usersService;
+    this.authService = authService;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signIn')
+  async signIn(
+    @Request() req,
+    @Response({ passthrough: true }) res,
+  ): Promise<string | never> {
+    const { accessToken, refreshToken } = await this.authService.signIn(
+      req.user,
+    );
+    res.cookie('refreshToken', refreshToken);
+
+    return Object.assign({
+      data: { accessToken },
+      statusCode: 201,
+      statusMsg: '로그인이 성공적으로 되었습니다.',
+    });
+  }
+
+  @UseGuards(JwtAuthGuard) //test
+  @Get('profile')
+  getProfile(@Request() req) {
+    console.log('controller');
+    return req.user;
   }
 
   @Post('signUp')
-  async saveUser(@Body() user: User): Promise<string> | never {
+  async saveUser(@Body() user: User): Promise<string | never> {
     const { email, nickname } = user;
-    let existedUser: User[] | [] = await this.usersService.findUserWithEmail(
-      email,
-    );
+    let existedUser = await this.usersService.findUserWithEmail(email);
 
-    if (existedUser.length) {
+    if (existedUser) {
       throw new HttpException(
         {
           data: null,
@@ -34,7 +68,7 @@ export class UsersController {
 
     existedUser = await this.usersService.findUserWithNickname(nickname);
 
-    if (existedUser.length) {
+    if (existedUser) {
       throw new HttpException(
         {
           data: null,
