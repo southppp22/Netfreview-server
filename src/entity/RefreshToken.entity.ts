7759ac286d4e527a5258b463d0e43ea843096079
southppp22 <tnddl032@gmail.com>
2021-02-26 03:33:02 +0900

    [Server]Add refresh token entity
    add refresh token entity

---
+++ src/entity/RefreshToken.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './User.entity';
+
+@Entity()
+export class RefreshToken {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @OneToOne(() => User, (user) => user.refreshToken)
+  @JoinColumn()
+  user: User;
+
+  @Column({ default: false })
+  isRevoked: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
