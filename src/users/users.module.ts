2d317b81384a6863a95d97cbc383517968c7f327
southppp22 <tnddl032@gmail.com>
2021-02-25 15:01:21 +0900

    [Server]Add AuthModule
    인증모듈 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { User } from 'src/entity/User.entity';
 import { ReviewsService } from 'src/reviews/reviews.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
