f4bc761b3d49d2116fa48300b274c6d0bb4681ea
최재선 <js@choejaeseon-ui-MacBookPro.local>
2021-03-02 15:24:15 +0900

    [Server] API - VIDEO쪽 전부 구현
    VIDEO add, get, post 구현 및 aboutthis 로직 완성!

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,9 +1,11 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
   Param,
   Post,
+  Query,
   Request,
   UnauthorizedException,
   UseGuards,
@@ -12,6 +14,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ReviewsService } from 'src/reviews/reviews.service';
 import { UsersService } from 'src/users/users.service';
 import { VideosService } from './videos.service';
+import { TokenService } from '../auth/token.service';
 
 @Controller('videos')
 export class VideosController {
@@ -19,10 +22,63 @@ export class VideosController {
     private videosService: VideosService,
     private reviewsService: ReviewsService,
     private usersService: UsersService,
+    private tokenService: TokenService,
   ) {
     this.videosService = videosService;
     this.reviewsService = reviewsService;
     this.usersService = usersService;
+    this.tokenService = tokenService;
+  }
+
+  @Get('/videolist')
+  async getVideoList(
+    @Query('path') path: string,
+    @Query('q') q: string,
+    @Request() req,
+  ) {
+    if (path === 'myPage') {
+      const refreshToken = req.cookies.refreshToken;
+      if (!refreshToken) {
+        throw new UnauthorizedException('로그인 후 이용 가능합니다.');
+      }
+      const { user } = await this.tokenService.resolveRefreshToken(
+        req.cookies.refreshToken,
+      );
+      const videoList = await this.videosService.getUserVideo(user.id);
+      return Object.assign({
+        videoList: videoList,
+      });
+    } else if (path === 'aboutThis') {
+      const refreshToken = req.cookies.refreshToken;
+      if (!refreshToken) {
+        throw new UnauthorizedException('로그인 후 이용 가능합니다.');
+      }
+      const { user } = await this.tokenService.resolveRefreshToken(
+        req.cookies.refreshToken,
+      );
+      const userId = user.id;
+      const videoList = await this.videosService.getUserVideo(userId);
+      const videoIds = [];
+      for (const video of videoList) {
+        videoIds.push(video.id);
+      }
+      const aboutThisVid = await this.videosService.getUserAboutThis(
+        videoIds,
+        userId,
+      );
+      return Object.assign({
+        videoList: aboutThisVid,
+      });
+    } else if (path === 'all') {
+      const allVideolist = await this.videosService.getSearchVideo(q);
+      return Object.assign({
+        videoList: allVideolist,
+      });
+    } else {
+      throw new BadRequestException(
+        '잘못된 경로의 요청입니다. Path를 확인해주세요',
+      );
+    }
   }
 
   @Get(':videoId')
@@ -31,7 +87,16 @@ export class VideosController {
     const avgRating = await this.reviewsService.getThisVidReviewAvgRate(
       videoId,
     );
-    console.log(rawVideoData, avgRating);
+    const genres = await this.videosService.getThisVidGenreWithId(videoId);
+    const genreBucket = [];
+    for (const genre of genres) {
+      genreBucket.push(genre.name);
+    }
+    return Object.assign({
+      ...rawVideoData,
+      rating: avgRating,
+      genres: genreBucket,
+    });
   }
 
   @UseGuards(JwtAuthGuard)
--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -6,12 +6,15 @@ import { VideosController } from './videos.controller';
 import { ReviewsModule } from 'src/reviews/reviews.module';
 import { UsersModule } from 'src/users/users.module';
 import { Genre } from 'src/entity/Genre.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { Review } from 'src/entity/Review.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Video, Genre]),
+    TypeOrmModule.forFeature([Video, Genre, Review]),
     forwardRef(() => ReviewsModule),
     UsersModule,
+    AuthModule,
   ],
   controllers: [VideosController],
   providers: [VideosService],
