138a8059a3f708548b3eae7479d78b842af3edb0
10o0o <70881850+10o0o@users.noreply.github.com>
2021-03-05 17:30:36 +0900

    Delete dist directory
    Server 배포용 파일 삭제

--- dist/reviews/dto/saveReviewDto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.saveReviewDto = void 0;
-class saveReviewDto {
-}
-exports.saveReviewDto = saveReviewDto;
-//# sourceMappingURL=saveReviewDto.js.map
