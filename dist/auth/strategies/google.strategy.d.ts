077d652986a6c9c78e4feaffebe9814564cf9b97
10o0o <70881850+10o0o@users.noreply.github.com>
2021-03-11 05:49:09 +0900

    Delete dist directory

--- dist/auth/strategies/google.strategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { Strategy } from 'passport-google-oauth20';
-import { AuthService } from '../auth.service';
-declare const GoogleStrategy_base: new (...args: any[]) => Strategy;
-export declare class GoogleStrategy extends GoogleStrategy_base {
-    private authService;
-    constructor(authService: AuthService);
-    validate(accessToken: string, refreshToken: string, profile: any): Promise<any>;
-}
-export {};
