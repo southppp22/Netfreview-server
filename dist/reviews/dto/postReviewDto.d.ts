afd9a5f75e497e449924d03717fec4adc75998c5
10o0o <70881850+10o0o@users.noreply.github.com>
2021-03-12 06:11:35 +0900

    Delete dist directory

--- dist/reviews/dto/postReviewDto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class ReviewDto {
-    text: string;
-    videoId: number;
-    rating: number;
-}
--- dist/videos/dto/videoDto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VideoDto = void 0;
-class VideoDto {
-}
-exports.VideoDto = VideoDto;
-//# sourceMappingURL=videoDto.js.map
